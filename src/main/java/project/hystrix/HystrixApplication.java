4af10554954b0f2219214c826c8823e69c6afd29
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-18 12:55:40 +0100

    done

--- src/main/java/project/hystrix/HystrixApplication.java
+++ src/main/java/project/hystrix/HystrixApplication.java
@@ -2,8 +2,13 @@ package project.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaClient
 public class HystrixApplication {
 
     public static void main(String[] args) {
